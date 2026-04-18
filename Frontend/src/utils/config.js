848b5b728b9a1ff3a1d8e96c3c0d4cd4c243c389
akshayrajput81266 <akshayrajput81266@gmail.com>
2026-04-18 16:10:51 +0530

    Update config.js

--- Frontend/src/utils/config.js
+++ Frontend/src/utils/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4001";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL || "https://book-store-website-backend-hh97.onrender.com";
 
 export default BASE_URL;
